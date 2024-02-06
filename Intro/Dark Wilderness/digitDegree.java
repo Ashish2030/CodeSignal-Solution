b8ee8549ca439790c286ea62f94b77e5723c60f4
kalyan Mekala <mekala.kalyan@gmail.com>
2024-02-06 17:32:50 -0500

    Updated digitDegree.java

--- Intro/Dark Wilderness/digitDegree.java
+++ Intro/Dark Wilderness/digitDegree.java
@@ -1,22 +1,28 @@
-int digitDegree(int n)
- {
-     int k=n;
-     
-     int sum=0;
-     int count=0;
-     while(String.valueOf(k).length()>1)
-     {
-         int temp=k;
-         sum=0;
-         count++;
-     while(temp>0)
-     {
-         sum=sum+temp%10;
-         temp=temp/10;
-     }
-     k=sum;
-     
-     }
-    return count;
-
+public class Main {
+    public static void main(String[] args) {
+        int n = 12345;
+        int result = digitDegree(n);
+        System.out.println("Digit degree: " + result);
+    }
+    
+    public static int digitDegree(int n) {
+        int k = n;
+        int sum = 0;
+        int count = 0;
+        
+        while (String.valueOf(k).length() > 1) {
+            int temp = k;
+            sum = 0;
+            count++;
+            
+            while (temp > 0) {
+                sum = sum + temp % 10;
+                temp = temp / 10;
+            }
+            
+            k = sum;
+        }
+        
+        return count;
+    }
 }
