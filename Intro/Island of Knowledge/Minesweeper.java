60402446a2cab611c08f897e56dde1aa4300c9f7
kalyan Mekala <mekala.kalyan@gmail.com>
2024-02-07 12:19:09 -0500

    Update Minesweeper.java

--- Intro/Island of Knowledge/Minesweeper.java
+++ Intro/Island of Knowledge/Minesweeper.java
@@ -1,47 +1,70 @@
-int[][] minesweeper(boolean[][] matrix) 
-{
-  
+/*The given code implements a Minesweeper game. It takes a boolean matrix as input, 
+where true represents a bomb and false represents an empty cell. The code iterates over 
+each cell in the matrix, counts the number of neighboring bombs for each cell, 
+and stores the counts in a new integer matrix. Finally, it returns the matrix 
+with the bomb counts and prints it to the console.*/
+
+public class Minesweeper {
+    public static int[][] minesweeper(boolean[][] matrix) {
         int[][] a = new int[matrix.length][matrix[0].length];
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 int bomb = 0;
-                if(j - 1 >= 0){ // check left
-                    if(matrix[i][j - 1])
+                if (j - 1 >= 0) { // check left
+                    if (matrix[i][j - 1])
                         bomb++;
                 }
-                if(j + 1 < matrix[i].length){ // check right
-                    if(matrix[i][j + 1])
+                if (j + 1 < matrix[i].length) { // check right
+                    if (matrix[i][j + 1])
                         bomb++;
                 }
-                if(i - 1 >= 0){ // check up
-                    if(matrix[i - 1][j])
+                if (i - 1 >= 0) { // check up
+                    if (matrix[i - 1][j])
                         bomb++;
                 }
-                if(i + 1 < matrix.length){ // check down
-                    if(matrix[i + 1][j])
+                if (i + 1 < matrix.length) { // check down
+                    if (matrix[i + 1][j])
                         bomb++;
                 }
-                if(i - 1 >= 0 && j - 1 >= 0){ // check upper left
-                    if(matrix[i - 1][j - 1])
+                if (i - 1 >= 0 && j - 1 >= 0) { // check upper left
+                    if (matrix[i - 1][j - 1])
                         bomb++;
                 }
-                if(i - 1 >= 0 && j + 1 < matrix[i].length){ // check upper right
-                    if(matrix[i - 1][j + 1])
+                if (i - 1 >= 0 && j + 1 < matrix[i].length) { // check upper right
+                    if (matrix[i - 1][j + 1])
                         bomb++;
                 }
-                if(i + 1 < matrix.length && j - 1 >= 0){ // check down left
-                    if(matrix[i + 1][j - 1])
+                if (i + 1 < matrix.length && j - 1 >= 0) { // check down left
+                    if (matrix[i + 1][j - 1])
                         bomb++;
                 }
-                if(i + 1 < matrix.length && j + 1 < matrix[i].length){ // check down right
-                    if(matrix[i + 1][j + 1])
+                if (i + 1 < matrix.length && j + 1 < matrix[i].length) { // check down right
+                    if (matrix[i + 1][j + 1])
                         bomb++;
                 }
                 a[i][j] = bomb;
-                bomb = 0;
             }
         }
 
         return a;
     }
+
+    public static void main(String[] args) {
+        boolean[][] matrix = {
+            {true, false, false, false},
+            {false, true, false, false},
+            {false, false, false, true},
+            {false, false, true, false}
+        };
+
+        int[][] result = minesweeper(matrix);
+
+        for (int[] row : result) {
+            for (int cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+}
