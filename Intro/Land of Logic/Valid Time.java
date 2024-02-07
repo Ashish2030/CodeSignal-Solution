e8a5525a7e2d59dbe833d030a1b434158ea8376f
kalyan Mekala <mekala.kalyan@gmail.com>
2024-02-07 15:09:34 -0500

    Update Valid Time.java

--- Intro/Land of Logic/Valid Time.java
+++ Intro/Land of Logic/Valid Time.java
@@ -1,40 +1,41 @@
-boolean validTime(String time)
- {
-     String a[]=time.split(":");
-     int a1=a.length;
-     if(a1!=2)
-     {
-         return false;
-     }
-     else
-     {
-         for(int i=0;i<a1;i++)
-         {
-             String y=a[i];
-             if(i==0)
-             {
-                 if(Integer.valueOf(y)>=0&&Integer.valueOf(y)<=23)
-                 {
-                     
-                 }
-                 else
-                 {
-                     return false;
-                 }
-             }
-             else
-             {
-                 if(Integer.valueOf(y)>=0&&Integer.valueOf(y)<=59)
-                 {
-                     
-                 }
-                 else
-                 {
-                     return false;
-                 } 
-             }
-         }
-     }
-     return true;
+/*The validTime method checks if a given time string is in the format "HH:MM". 
+It splits the string by the colon and verifies that there are exactly two parts. 
+It then checks if the hour part is between 0 and 23, and if the minute part is between 0 and 59. 
+If all conditions are met, the method returns true; otherwise, it returns false*/
 
+public class Main {
+    public static void main(String[] args) {
+        String time = "15:45";
+        boolean isValid = validTime(time);
+        System.out.println("Is the time valid? " + isValid);
+    }
+    
+    static boolean validTime(String time) {
+        String[] a = time.split(":");
+        int a1 = a.length;
+
+        if (a1 != 2) {
+            return false;
+        } else {
+            for (int i = 0; i < a1; i++) {
+                String y = a[i];
+
+                if (i == 0) {
+                    if (Integer.parseInt(y) >= 0 && Integer.parseInt(y) <= 23) {
+                        // Valid hour value
+                    } else {
+                        return false;
+                    }
+                } else {
+                    if (Integer.parseInt(y) >= 0 && Integer.parseInt(y) <= 59) {
+                        // Valid minute value
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
 }
