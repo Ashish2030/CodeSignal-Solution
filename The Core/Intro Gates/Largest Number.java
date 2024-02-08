0b13a0d27269d655a6604334159974e402cd3b42
kalyan Mekala <mekala.kalyan@gmail.com>
2024-02-08 14:10:13 -0500

    Update Largest Number.java

--- The Core/Intro Gates/Largest Number.java
+++ The Core/Intro Gates/Largest Number.java
@@ -1,6 +1,13 @@
-int largestNumber(int n)
- {
-     int x=(int)Math.pow(10,n);
-     return x-1;
+public class Main {
+    public static int largestNumber(int n) {
+        int x = (int) Math.pow(10, n);
+        return x - 1;
+    }
 
+    public static void main(String[] args) {
+        // Example usage
+        int n = 3;
+        int result = largestNumber(n);
+        System.out.println("Result: " + result);
+    }
 }
