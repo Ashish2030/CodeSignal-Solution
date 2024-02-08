05c3462ba83bf5f06a6f9b908cb3c2a560e76bd3
kalyan Mekala <mekala.kalyan@gmail.com>
2024-02-08 10:13:20 -0500

    Update Extra Number.java

--- The Core/At the Crossroads/Extra Number.java
+++ The Core/At the Crossroads/Extra Number.java
@@ -1,13 +1,20 @@
-int extraNumber(int a, int b, int c)
- {
-     if(a==b)
-     {
-         return c;
-     }
-     if(b==c)
-     {
-         return a;
-     }
-     return b;
+public class Main {
+    public static int extraNumber(int a, int b, int c) {
+        if (a == b) {
+            return c;
+        }
+        if (b == c) {
+            return a;
+        }
+        return b;
+    }
 
+    public static void main(String[] args) {
+        // Example usage
+        int a = 2;
+        int b = 2;
+        int c = 3;
+        int result = extraNumber(a, b, c);
+        System.out.println("Result: " + result);
+    }
 }
