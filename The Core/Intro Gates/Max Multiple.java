fbe84a694fe61843e1cf83e02b142e33fb33d505
kalyan Mekala <mekala.kalyan@gmail.com>
2024-02-08 14:55:11 -0500

    Update Max Multiple.java

--- The Core/Intro Gates/Max Multiple.java
+++ The Core/Intro Gates/Max Multiple.java
@@ -1,7 +1,14 @@
-int maxMultiple(int divisor, int bound)
- {
-     double a=(double)bound;
-     int b=(int)Math.floor(a/divisor);
-     return b*divisor;
+class Main {
+    public static void main(String[] args) {
+        int divisor = 3;
+        int bound = 10;
+        int result = maxMultiple(divisor, bound);
+        System.out.println("Result: " + result);
+    }
 
+    static int maxMultiple(int divisor, int bound) {
+        double a = (double) bound;
+        int b = (int) Math.floor(a / divisor);
+        return b * divisor;
+    }
 }
