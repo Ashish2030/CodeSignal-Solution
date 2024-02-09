cf7f3a9798632b97790c9d011d04f03515845e89
kalyan Mekala <mekala.kalyan@gmail.com>
2024-02-08 20:27:15 -0500

    Update Phone Call.java

--- The Core/Intro Gates/Phone Call.java
+++ The Core/Intro Gates/Phone Call.java
@@ -1,20 +1,26 @@
-int phoneCall(int min1, int min2_10, int min11, int s)
- {
-     int count=0;
-     if(s>=min1)
-     {
-         s=s-min1;
-         count++;
-     }
- while(s>=min2_10&&count<10)
- {
-     s=s-min2_10;
-     count++;
- }
-if(s>min11)
-{
-    count=count+s/min11;
-}
- return count;
+public class PhoneCall {
+    public static int phoneCall(int min1, int min2_10, int min11, int s) {
+        int count = 0;
+        if (s >= min1) {
+            s = s - min1;
+            count++;
+        }
+        while (s >= min2_10 && count < 10) {
+            s = s - min2_10;
+            count++;
+        }
+        if (s > min11) {
+            count = count + (s / min11);
+        }
+        return count;
+    }
 
+    public static void main(String[] args) {
+        int min1 = 1;
+        int min2_10 = 2;
+        int min11 = 3;
+        int s = 20;
+        int result = phoneCall(min1, min2_10, min11, s);
+        System.out.println("Total minutes: " + result);
+    }
 }
